ca0cac99d70d19710864fbf336273765960792ee
OhmPatil <workpatil10@gmail.com>
2022-05-27 14:49:41 +0530

    Work as expected

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-window.addEventListener('keydown', function(e){
-    // console.log(e)
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    console.log(key)
-    // console.log(audio)
-    if (!audio) return
-    audio.currentTime=0;
-    audio.play()
+// // console.log(e)
+// const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+// const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+// // console.log(key)
+// // console.log(audio)
+// if (!audio) return;
+// audio.currentTime=0;
+// audio.play();
+// key.classList.add('playing');
+// });
 
-    key.classList.add('playing')
-})
+function play(e){
+    // console.log(e)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    // console.log(key)
+    // console.log(audio)
+    if (!audio) return;
+    audio.currentTime=0;
+    audio.play();
+    key.classList.add('playing');
+}
+function removeClass(e){
+    console.log(this)
+    this.classList.remove('playing')
+    
+}
+
+window.addEventListener('keydown', play)
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeClass));
